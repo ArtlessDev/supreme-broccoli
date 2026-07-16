2311b8d45d4d183568656beddee33c2fd2ee80e8
artlessDev <jayanlas00@gmail.com>
2026-07-15 23:33:38 -0400

    updated og tileset and the tilemaps, still need to export them to csvs

--- artwork/Visual/Tiled/tileset_original.tsx
+++ artwork/Visual/Tiled/tileset_original.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tileset_original" tilewidth="128" tileheight="128" tilecount="256" columns="9">
- <image source="../../../SupremeBroccoli/SupremeBroccoli/Content/tilemaps/tileset_original.png" width="1152" height="1024"/>
+<tileset version="1.10" tiledversion="1.11.0" name="tileset_original" tilewidth="128" tileheight="128" tilecount="130" columns="10">
+ <image source="../../../SupremeBroccoli/SupremeBroccoli/Content/tilemaps/tileset_original.png" width="1280" height="1778"/>
 </tileset>
